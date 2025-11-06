56e2ba5d182f92952179d59eb62a635a0f7ae551
riyudev <virtudazoy@gmail.com>
2025-11-06 11:29:33 +0800

    added breadcrumbs

---
+++ Frontend/src/Components/BreadCrumbs.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { IoChevronForward } from "react-icons/io5";
+
+const Breadcrumbs = ({ product }) => {
+  if (!product) return null;
+
+  return (
+    <div className="mb-6 flex w-full items-center text-sm text-gray-600">
+      {/* Home */}
+      <Link to="/" className="flex items-center transition hover:text-black">
+        <FaHome className="mr-1 text-gray-500" />
+        Home
+      </Link>
+
+      {/* Divider */}
+      <IoChevronForward className="mx-2 text-gray-400" />
+
+      {/* Category */}
+      <Link
+        to={`/${product.category}`}
+        className="capitalize transition hover:text-black"
+      >
+        {product.category}
+      </Link>
+
+      {/* Divider */}
+      <IoChevronForward className="mx-2 text-gray-400" />
+
+      {/* Product Name */}
+      <span className="font-medium text-black">{product.name}</span>
+    </div>
+  );
+};
+
+export default Breadcrumbs;
